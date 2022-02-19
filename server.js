158f0fb501bf8c9fd5692c785378cc8bbdec27d4
Ryo Suzuki <ryosuzk@gmail.com>
2022-02-19 12:08:43 -0700

    Fix

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const socketio = require('socket.io')
 const http = require('http')
 const path = require('path')
-const { NearScanner } = require('@toio/scanner')
+const { NearestScanner } = require('@toio/scanner')
 const app = express()
 const server = http.Server(app)
 const io = socketio(server)
@@ -28,17 +28,10 @@ io.on('connection', (socket) => {
 })
 
 let num = 1
-let cubes = new Array(num)
 async function init(io) {
-  try {
-    cubes = await new NearScanner(num).start()
-    for (let i = 0; i < num; i++) {
-      cubes[i].connect()
-    }
-    setInterval(() => {
-      io.sockets.emit('pos', { cubes: cubes })
-    }, 1000)
-  } catch (err) {
-    console.log(err)
-  }
+  let cube = await new NearestScanner().start()
+  cube.connect()
+  setInterval(() => {
+    io.sockets.emit('pos', { cubes: [cube] })
+  }, 1000)
 }
